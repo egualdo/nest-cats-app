a65c70852237b386438d19f6c773113203c48adb
egualdo <colmenares203@gmail.com>
2026-04-08 15:38:18 -0400

    forward ref to RolesModule

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,7 +6,7 @@ import { Role } from './entities/role.entity';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Role]), forwardRef(() => AuthModule)],
   controllers: [RolesController],
   providers: [RolesService],
   exports: [TypeOrmModule],
