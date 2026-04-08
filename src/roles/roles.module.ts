02806c9f67a49633dcd00a36b415f36331a54852
egualdo <colmenares203@gmail.com>
2026-04-08 15:30:16 -0400

    modifying modules for using AuthModule (RoleModule)

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -3,9 +3,10 @@ import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role])],
+  imports: [TypeOrmModule.forFeature([Role]), AuthModule],
   controllers: [RolesController],
   providers: [RolesService],
   exports: [TypeOrmModule],
