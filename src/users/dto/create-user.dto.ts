d62c7f0858b868639cdd4379a292065ef7bd211a
egualdo <colmenares203@gmail.com>
2026-04-13 14:08:22 -0400

    roleId change for string compare enum into controller 2

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,7 +3,6 @@ import { IsNumber, IsString } from "class-validator";
 export class CreateUserDto {
     @IsString()
     name!: string;//el ! indica que se inicializará posteriormente, es decir, no es necesario asignarle un valor en el momento de la declaración
-
     @IsString()
     email!: string;
     @IsString()
