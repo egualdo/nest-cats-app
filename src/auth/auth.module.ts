788daa8d9465e030b067ce9fa260e43ab295acfe
egualdo <colmenares203@gmail.com>
2026-04-13 11:12:26 -0400

    role relationship with user table fixing 7

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,15 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt'; //se importa el JwtModule para poder usarlo en el AuthService
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RolesService } from 'src/roles/roles.service';
+import { RolesModule } from 'src/roles/roles.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     UsersModule,
+    RolesModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -19,9 +22,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
       inject: [ConfigService],
     }),
+    RolesService
   ],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [JwtModule] //exportamos el AuthService para poder usarlo en otros modulos, como el UsersModule por ejemplo
+  exports: [JwtModule, RolesService] //exportamos el AuthService para poder usarlo en otros modulos, como el UsersModule por ejemplo
 })
 export class AuthModule { }
