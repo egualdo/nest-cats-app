d52bb913be9f991f41546dda04de92ad4916d9a7
egualdo <colmenares203@gmail.com>
2026-04-13 19:42:06 -0400

    import rolesService  to catModule

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -7,11 +7,13 @@ import { BreedsModule } from 'src/breeds/breeds.module';
 import { BreedsService } from 'src/breeds/breeds.service';
 import { Breed } from 'src/breeds/entities/breed.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { RolesModule } from 'src/roles/roles.module';
+import { RolesService } from 'src/roles/roles.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cat]), BreedsModule, AuthModule],// importamos el modulo de las razas para poder usar el services
+  imports: [TypeOrmModule.forFeature([Cat]), BreedsModule, AuthModule, RolesModule],// importamos el modulo de las razas para poder usar el services
   controllers: [CatsController],
-  providers: [CatsService, BreedsService],
+  providers: [CatsService, BreedsService, RolesService],
   exports: [],
 })
 export class CatsModule { }
