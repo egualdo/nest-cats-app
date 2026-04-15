99bd6de62f08ba4d73633e320b9d67125a2cd336
egualdo <colmenares203@gmail.com>
2026-04-15 09:46:15 -0400

    adding swagger tags to Cat controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -7,13 +7,22 @@ import { Auth } from '../auth/decorators/auth.decorator';
 import { Role } from '../common/enums/role.enum';
 import { ActiveUser } from '../common/decorators/active-user.decorator';
 import type { ActiveUserInterface } from '../common/interfaces/active-user.interface';
-
+import { ApiBearerAuth, ApiCreatedResponse, ApiForbiddenResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+@ApiTags('cats')
+@ApiBearerAuth()
+@ApiUnauthorizedResponse({
+  description: 'Unauthorized Bearer Auth',
+})
 @Auth(Role.USER)//se usa para proteger todas las rutas del controlador, solo los usuarios con el rol de USER podran acceder a estas rutas, si queremos que solo los ADMIN puedan acceder a estas rutas, cambiamos Role.USER por Role.ADMIN
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) { }
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
   create(@Body() createCatDto: CreateCatDto, @ActiveUser() user: ActiveUserInterface) {
     return this.catsService.create(createCatDto, user);
   }
