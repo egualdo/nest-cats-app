05e84312435d7c9ead4abcb5e9293089fa6431d7
egualdo <colmenares203@gmail.com>
2026-04-15 09:35:35 -0400

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
@@ -17,6 +18,14 @@ async function bootstrap() {
       //  si el DTO lo especifica
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("Cats example")
+    .setDescription("The cats API description")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
